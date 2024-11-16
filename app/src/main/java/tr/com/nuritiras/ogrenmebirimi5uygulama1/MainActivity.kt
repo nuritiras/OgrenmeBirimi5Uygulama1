20d33f687d452310b49fc3299489c02428fa82f5
Nuri TIRAŞ <nuritiras@gmail.com>
2024-11-16 23:44:49 +0300

    Void Methods

---
+++ app/src/main/java/tr/com/nuritiras/ogrenmebirimi5uygulama1/MainActivity.kt
@@ -0,0 +1,30 @@
+package tr.com.nuritiras.ogrenmebirimi5uygulama1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        findViewById<Button>(R.id.button1).setOnClickListener {selamVer()}
+        findViewById<Button>(R.id.button2).setOnClickListener {selamVer()}
+
+    }
+
+    private fun selamVer() {
+        Toast.makeText(this, "Merhaba", Toast.LENGTH_SHORT).show()
+    }
+}
